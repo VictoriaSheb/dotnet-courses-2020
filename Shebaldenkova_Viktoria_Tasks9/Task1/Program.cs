9f42beb2ba91eb7c7095d7b27c12e12b7028ee2c
79156 <79156@LAPTOP-VL8K1AA5>
2020-10-20 01:48:32 +0300

    Solution9_Creation

---
+++ Shebaldenkova_Viktoria_Tasks9/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> people1 = RamdomList(7);
+            RemoveEachSecondItem(ref people1);
+            LinkedList<int> people2 = RamdomLinkedList(7);
+            RemoveEachSecondItem(ref people2);
+            Console.WriteLine("List<T> вычеркивание: "+ people1.First());
+            Console.WriteLine("LinkedList<T> вычеркивание: " + people2.First());
+            Console.ReadLine();
+
+        }
+
+        public static List<int> RamdomList(int n)
+        {
+            List<int> people = new List<int>(n);
+            for (int i=0;i<n;i++) 
+            {
+                people.Add(i+1);
+            }
+            return people;
+        }
+
+        public static LinkedList<int> RamdomLinkedList(int n)
+        {
+            LinkedList<int> people = new LinkedList<int>();
+            for (int i = 0; i < n; i++)
+            {
+                people.AddLast(i + 1);
+            }
+            return people;
+        }
+
+        public static void RemoveEachSecondItem(ref List<int> list) 
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                list.RemoveAt(i);
+                if (list.Count == 1)
+                    break;
+                if ((i + 1) == list.Count)
+                    i = -1;
+                if ((i + 1) > list.Count)
+                    i = 0;
+            }
+            
+        }
+        public static void RemoveEachSecondItem(ref LinkedList<int> list)
+        {
+            int element = list.First.Next.Value;
+            int elementNext = list.First.Next.Next.Value;
+            while (list.Count != 1)
+            {
+                list.Remove(element);
+                if (list.Count == 1)
+                    break;
+                element = (list.Find(elementNext).Next ?? list.First).Value;
+                elementNext = (list.Find(element).Next ?? list.First).Value;
+            }
+
+        }
+    }
+}
+
---
+++ Shebaldenkova_Viktoria_Tasks9/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\b([a-z])(\w+?)\b", RegexOptions.IgnoreCase);
+            string text = @"AAa aaa ere Ere, you uuu uuuu uuu .//,.uuu";
+            int count = 0;
+            MatchCollection matches = regex.Matches(text.ToLower());
+            string[] words = new string[matches.Count];
+            foreach (var match in matches)
+            {
+                words[count++]=match.ToString();
+            } 
+            for (int i = 0; i < words.Length; i++) 
+            {
+                count = 1;
+                if (words[i] != null)
+                {
+                    for (int j = i+1; j < words.Length; j++)
+                    {
+                        if (words[i] == words[j])
+                        {
+                            count++;
+                            words[j] = null;
+                        }
+                    }
+                    Console.WriteLine("'{0}' повторяется {1} раз", words[i], count);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
